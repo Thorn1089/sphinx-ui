8f963a48b5da855b05faff3fe4c7b08d8c3c10f8
Tom Golden <TomRK1089@gmail.com>
2014-06-29 17:10:09 -0400

    Added a generic QuestionController interface.

---
+++ src/main/java/com/atomiccomics/sphinx/ui/survey/QuestionController.java
@@ -0,0 +1,22 @@
+package com.atomiccomics.sphinx.ui.survey;
+
+import javafx.beans.binding.BooleanBinding;
+
+/**
+ * The {@link QuestionController} defines a common interface for all
+ * question types that are represented visually within the Sphinx application.
+ * They can expose a simple {@link BooleanBinding} property that represents whether
+ * the currently supplied answer is valid or invalid.
+ * 
+ * @author Tom
+ *
+ */
+public interface QuestionController {
+
+	/**
+	 * @return A {@link BooleanBinding} representing whether the current answer
+	 * encapsulated by this controller is valid or not.
+	 */
+	BooleanBinding validProperty();
+	
+}
