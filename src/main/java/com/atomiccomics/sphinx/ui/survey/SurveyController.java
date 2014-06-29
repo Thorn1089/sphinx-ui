14c3cd0effdad7be6ba4649f9a3b1cab60362e0c
Tom Golden <TomRK1089@gmail.com>
2014-06-29 17:11:28 -0400

    Updated the SurveyController to properly bind the next button to the valid property for QuestionController types.

--- src/main/java/com/atomiccomics/sphinx/ui/survey/SurveyController.java
+++ src/main/java/com/atomiccomics/sphinx/ui/survey/SurveyController.java
@@ -2,13 +2,17 @@ package com.atomiccomics.sphinx.ui.survey;
 
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.function.Consumer;
 
 import javax.inject.Inject;
 
+import javafx.beans.binding.Bindings;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.Node;
 
@@ -16,6 +20,7 @@ import com.atomiccomics.sphinx.ui.main.CloseEvent;
 import com.atomiccomics.survey.common.FillInNumberQuestion;
 import com.atomiccomics.survey.common.Instructions;
 import com.atomiccomics.survey.common.StaticSection;
+import com.atomiccomics.survey.common.TrueFalseQuestion;
 import com.atomiccomics.survey.core.Question;
 import com.atomiccomics.survey.core.Section;
 import com.atomiccomics.survey.engine.SurveyBlackboard;
@@ -34,6 +39,12 @@ public class SurveyController {
 	@FXML
 	private ScrollPane contentPane;
 	
+	@FXML
+	private Button back;
+	
+	@FXML
+	private Button forward;
+	
 	private SurveyDriver driver;
 	
 	@Inject
@@ -47,19 +58,20 @@ public class SurveyController {
 	public void loadSurvey(final Path survey, final SurveyBlackboard blackboard) {
 		//TODO Actually parse...for now, let's fake a survey with sample instructions
 		final Instructions demoInstructions = new Instructions("I-01", (bb) -> true, "Please read these carefully");
-		final Instructions second = new Instructions("I-02", (bb) -> true, "Did you really read them carefully? Go back.");
+		final TrueFalseQuestion checkUnderstanding = new TrueFalseQuestion("I-02", (bb) -> true, "Did you really read them carefully?");
 		final FillInNumberQuestion age = new FillInNumberQuestion("D-01", (bb) -> true, "How old are you?");
-		final Section section = new StaticSection((bb) -> true, Arrays.asList(demoInstructions, second, age));
+		final Section section = new StaticSection((bb) -> true, Arrays.asList(demoInstructions, checkUnderstanding, age));
 		driver = new SurveyDriver(section, (question) -> {
 			final LoadQuestionTask task = new LoadQuestionTask(question);
 			task.setOnSucceeded((event) -> {
 				try {
-					contentPane.setContent(task.get());
-				} catch (Exception e) {
+					task.get().accept(contentPane);
+				} catch (InterruptedException | ExecutionException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			});
+			task.setOnFailed((event) -> task.getException().printStackTrace());
 			worker.submit(task);
 		}, () -> {
 			eventBus.post(new CloseEvent());
@@ -78,7 +90,7 @@ public class SurveyController {
 		driver.previous();
 	}
 	
-	private final class LoadQuestionTask extends Task<Node> {
+	private final class LoadQuestionTask extends Task<Consumer<ScrollPane>> {
 
 		private final Question question;
 		
@@ -87,21 +99,36 @@ public class SurveyController {
 		}
 		
 		@Override
-		protected Node call() throws Exception {
+		protected Consumer<ScrollPane> call() throws Exception {
 			if(question instanceof Instructions) {
 				final FXMLLoader loader = loaderProvider.get();
 				final Node panel = loader.load(SurveyController.class.getResourceAsStream("Instructions.fxml"));
 				final InstructionsController instructions = loader.getController();
-				instructions.setInstructions((Instructions)question);
-				return panel;
+				return (scrollPane) -> {
+					instructions.setInstructions((Instructions)question);
+					forward.disableProperty().bind(Bindings.not(instructions.validProperty()));
+					scrollPane.setContent(panel);
+				};
 			} else if(question instanceof FillInNumberQuestion) {
 				final FXMLLoader loader = loaderProvider.get();
 				final Node panel = loader.load(SurveyController.class.getResourceAsStream("FillInNumber.fxml"));
 				final FillInNumberController fillInNumber = loader.getController();
-				fillInNumber.displayQuestion((FillInNumberQuestion)question);
-				return panel;
+				return (scrollPane) -> {
+					fillInNumber.displayQuestion((FillInNumberQuestion)question);
+					forward.disableProperty().bind(Bindings.not(fillInNumber.validProperty()));
+					scrollPane.setContent(panel);
+				};
+			} else if(question instanceof TrueFalseQuestion) {
+				final FXMLLoader loader = loaderProvider.get();
+				final Node panel = loader.load(SurveyController.class.getResourceAsStream("TrueFalse.fxml"));
+				final TrueFalseController trueFalse = loader.getController();
+				return (scrollPane) -> {
+					trueFalse.displayQuestion((TrueFalseQuestion)question);
+					forward.disableProperty().bind(Bindings.not(trueFalse.validProperty()));
+					scrollPane.setContent(panel);
+				};
 			} else {
-				return null;
+				return (scrollPane) -> {};
 			}
 		}
 		
